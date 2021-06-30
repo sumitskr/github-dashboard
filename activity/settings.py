8c5e680ad0b91e95fdf4746206bca4098b0324ef
sumitskr <sumitsarkar338@gmail.com>
2021-06-30 23:44:47 +0530

    rest

--- activity/settings.py
+++ activity/settings.py
@@ -13,7 +13,7 @@ import django_heroku
 from pathlib import Path
 import dj_database_url
 from decouple import config 
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'km53#5_!u9&c07fml^n&@0^8=j+#(nuirsdp+!-mh5qh#=6g2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sumitapi2.herokuapp.com']
 
@@ -142,6 +142,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 # Activate Django-Heroku.
