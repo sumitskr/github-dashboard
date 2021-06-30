f16160619bca162658bf8660679f1bf91c7ec6a4
sumitskr <sumitsarkar338@gmail.com>
2021-06-30 21:05:05 +0530

    rest

--- activity/settings.py
+++ activity/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import dj_database_url
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
