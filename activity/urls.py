43ac21e995570c216fe1a4b58ad50e8f956a3aeb
sumitskr <sumitsarkar338@gmail.com>
2021-06-30 23:50:46 +0530

    rest

--- activity/urls.py
+++ activity/urls.py
@@ -1,4 +1,4 @@
-"""activity URL Configuration
+"""activity URL 
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
